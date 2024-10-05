402f3b1978506bdc0e33dd27976794a90a598a2b
ismailsobhy <32613878+ismailsobhy@users.noreply.github.com>
2024-10-05 19:22:44 +0300

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  globalTimeout: 30000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://my-stage.tractive.com",
